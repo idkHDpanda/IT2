630327aafae24e0003050dfb0268db0c2bfa121c
idkHDpanda <annapham_@live.no>
2019-09-03 08:57:20 +0200

    VariablerOgOperatorer

---
+++ 2019/uke36-variablerOgOperatorer/variabler.js
@@ -0,0 +1,5 @@
+//Variabler i javascript
+
+//Deklarere en variabel (opprette), med nøkkelordet let
+let minAlder = 18;
+//Variabel navn, tilordningsoperatoren, verdi (Gir et bestemt navn til et verdi)
