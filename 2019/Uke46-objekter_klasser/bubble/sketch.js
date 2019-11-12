0ccb701635e9cbf55fa32e8ba54c3bd5bcbd1b9c
idkHDpanda <annapham_@live.no>
2019-11-12 08:52:01 +0100

    bubble

--- 2019/Uke46-objekter_klasser/bubble/sketch.js
+++ 2019/Uke46-objekter_klasser/bubble/sketch.js
@@ -1,27 +1,47 @@
-let bubble = {
-  x: 300,
-  y: 200,
-}
- 
+let bubble = [];
+
 function setup() {
   createCanvas(600, 400);
+//  for(let i = 0; i < 40; i++){
+//    let x = 20 + 100*i;
+//    bubble[i] = new Bubble(x, 200, 20);
+  }
 
+function mousePressed() { //mouseDragged
+  let r = random(10, 50);
+  let b = new Bubble(mouseX, mouseY,r)
+  bubble.push(b);
 }
 
 function draw() {
   background(0);
-  move();
-  display();
+  for(let i = 0; i < bubble.length; i++){
+    bubble[i]. move();
+    bubble[i]. show();
+//    bubble[i].sprett();
+  }
 }
 
-function display(){
-  stroke(255);
-  strokeWeight(4);
-  noFill();
-  ellipse(bubble.x, bubble.y, 24, 24);
-}
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
 
-function move() {
-  bubble.x = bubble.x + random(-5,5);
-  bubble.y = bubble.y + random(-5,5);
-}
+  move(){
+    this.x = this.x + random(-5,5);
+    this.y = this.y + random(-5,5);
+  }
+
+  show(){
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r*2);
+  }
+
+//  sprett(){
+//    this.x += Math.random()*10-5;
+//  }
+//}
